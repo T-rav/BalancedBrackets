21aaf6b00fa0d59d24a1a302e3c05d1b074b1447
T-rav <tmfrisinger@gmail.com>
2017-09-06 09:04:29 +0200

    First 3 test passing

---
+++ Brackets.Parser.Tests/BracketsParserTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace Brackets.Parser.Tests
+{
+    [TestFixture]
+    public class BracketsParserTests
+    {
+        [Test]
+        public void VerifyBracketsAreBalanced_WhenEmptyString_ShouldReturnOk()
+        {
+            //---------------Arrange-------------------
+            var input = string.Empty;
+            var expected = "Ok";
+            var parser = new BracketsParser();
+            //---------------Act ----------------------
+            var result = parser.VerifyBracketsAreBalanced(input);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected,result);
+        }
+
+        [TestCase("[")]
+        [TestCase("]")]
+        public void VerifyBracketsAreBalanced_WhenSingleBracket_ShouldReturnFail(string input)
+        {
+            //---------------Arrange-------------------
+            var expected = "Fail";
+            var parser = new BracketsParser();
+            //---------------Act ----------------------
+            var result = parser.VerifyBracketsAreBalanced(input);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void VerifyBracketsAreBalanced_WhenPairOfUnBalancedBrackets_ShouldReturnFail()
+        {
+            //---------------Arrange-------------------
+            var input = "][";
+            var expected = "Fail";
+            var parser = new BracketsParser();
+            //---------------Act ----------------------
+            var result = parser.VerifyBracketsAreBalanced(input);
+            //---------------Assert -----------------------
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    public class BracketsParser
+    {
+        public string VerifyBracketsAreBalanced(string input)
+        {
+            if (input == string.Empty)
+            {
+                return "Ok";
+            }
+            if (input == "[" || input == "]" || input == "][")
+            {
+                return "Fail";
+            }
+            return string.Empty;
+        }
+    }
+}
