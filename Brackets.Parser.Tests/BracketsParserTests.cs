1b9a80ee3b95f204ce0384903a0c896de69ac84a
T-rav <tmfrisinger@gmail.com>
2017-09-06 09:13:39 +0200

    Moved BracketsParser.cs into its own project

--- Brackets.Parser.Tests/BracketsParserTests.cs
+++ Brackets.Parser.Tests/BracketsParserTests.cs
@@ -83,22 +83,4 @@ namespace Brackets.Parser.Tests
             Assert.AreEqual(expected, result);
         }
     }
-
-    public class BracketsParser
-    {
-        public string VerifyBracketsAreBalanced(string input)
-        {
-            if (input.StartsWith("]") || input.EndsWith("[") || input.Length % 2 != 0)
-            {
-                return "Fail";
-            }
-
-            if (input.Length % 2 == 0)
-            {
-                return "Ok";
-            }
-
-            return string.Empty;
-        }
-    }
 }
---
+++ Brackets.Parser/BracketsParser.cs
@@ -0,0 +1,20 @@
+﻿namespace Brackets.Parser
+{
+    public class BracketsParser
+    {
+        public string VerifyBracketsAreBalanced(string input)
+        {
+            if (input.StartsWith("]") || input.EndsWith("[") || input.Length % 2 != 0)
+            {
+                return "Fail";
+            }
+
+            if (input.Length % 2 == 0)
+            {
+                return "Ok";
+            }
+
+            return string.Empty;
+        }
+    }
+}
